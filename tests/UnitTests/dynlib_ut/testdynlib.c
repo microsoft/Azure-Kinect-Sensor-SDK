7e2405b268954f4790b334fc39e61ea5b2a65212
Matt Schulte <maschu@microsoft.com>
2019-02-06 18:13:34 -0800

    Added void to prototype

--- tests/UnitTests/dynlib_ut/testdynlib.c
+++ tests/UnitTests/dynlib_ut/testdynlib.c
@@ -5,9 +5,9 @@
 #define __cdecl
 #endif
 
-__declspec(dllexport) void __cdecl say_hello();
+__declspec(dllexport) void __cdecl say_hello(void);
 
-__declspec(dllexport) void __cdecl say_hello()
+__declspec(dllexport) void __cdecl say_hello(void)
 {
     printf("Hello!\n");
 }
