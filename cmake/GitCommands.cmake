4b1f46ae9e688a72c3398758dc59e598febc211c
BrentA-Microsoft <brenta@microsoft.com>
2019-02-08 12:19:04 -0800

    Removed dynamic version number generation (#44)
    * Removed dynamic version number generation
    * Changed default version to 1.0.0-private

---
+++ cmake/GitCommands.cmake
@@ -0,0 +1,23 @@
+find_package(Git REQUIRED QUIET)
+if (NOT Git_FOUND)
+    message(FATAL_ERROR "Unable to find git, which is needed for versioning")
+endif()
+
+function(get_git_dir DIRECTORY OUTPUT_VAR)
+    execute_process(
+        COMMAND
+            ${GIT_EXECUTABLE} rev-parse --git-dir
+        WORKING_DIRECTORY
+            ${DIRECTORY}
+        RESULT_VARIABLE
+            GIT_DIR_RESULT
+        OUTPUT_VARIABLE
+            GIT_DIR_OUTPUT
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    if (GIT_DIR_OUTPUT)
+        set(${OUTPUT_VAR} ${GIT_DIR_OUTPUT} PARENT_SCOPE)
+    endif()
+endfunction()
