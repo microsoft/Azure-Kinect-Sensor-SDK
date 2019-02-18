8d2bf6ca6d1308cdf2cfdd893759c6e099eb157b
vujova <vujova@microsoft.om>
2019-02-18 21:39:02 +0100

    Implementing rest of the API...

--- include/k4a/k4a_cpp.h
+++ include/k4a/k4a_cpp.h
@@ -5,15 +5,53 @@
 #include <chrono>
 #include <cstdint>
 #include <limits>
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 namespace k4a
 {
+    class result_error : public std::runtime_error
+    {
+    public:
+        result_error(k4a_result_t result)
+            : std::runtime_error("K4A result not success")
+            , m_result(result)
+        {
+        }
+
+        k4a_result_t result() const
+        {
+            return m_result;
+        }
+
+    private:
+        k4a_result_t m_result;
+    };
+
+    class wait_result_error : public std::runtime_error
+    {
+    public:
+        wait_result_error(k4a_wait_result_t result)
+            : std::runtime_error("K4A wait result not success")
+            , m_result(result)
+        {
+        }
+
+        k4a_wait_result_t result() const
+        {
+            return m_result;
+        }
+
+    private:
+        k4a_wait_result_t m_result;
+    };
+
     inline void check_result(k4a_result_t result)
     {
         if (result != K4A_RESULT_SUCCEEDED)
         {
-            // TODO: Throw exception
+            throw result_error(result);
         }
     }
 
@@ -21,7 +59,7 @@ namespace k4a
     {
         if (result != K4A_WAIT_RESULT_SUCCEEDED)
         {
-            // TODO: Throw exception
+            throw wait_result_error(result);
         }
     }
 
@@ -94,6 +132,16 @@ namespace k4a
         operator bool() const
         {
             return m_handle != nullptr;
+        }
+
+        k4a_image_t handle()
+        {
+            return m_handle;
+        }
+
+        const k4a_image_t handle() const
+        {
+            return m_handle;
         }
 
         void release()
@@ -171,10 +219,7 @@ namespace k4a
                 buffer_release_cb_context,
                 img);
 
-            if (result != K4A_RESULT_SUCCEEDED)
-            {
-                // TODO: Throw exception
-            }
+            check_result(result);
             return img;
         }
 
@@ -238,7 +283,15 @@ namespace k4a
             k4a_image_set_exposure_time_usec(m_handle, timestamp.count());
         }
 
-        // TODO: Other set methods
+        void set_white_balance(uint32_t white_balance)
+        {
+            k4a_image_set_white_balance(m_handle, white_balance);
+        }
+
+        void set_iso_speed(uint32_t iso_speed)
+        {
+            k4a_image_set_iso_speed(m_handle, iso_speed);
+        }
 
     private:
         k4a_image_t m_handle;
@@ -339,11 +392,20 @@ namespace k4a
             return image(k4a_capture_get_ir_image(m_handle));
         }
 
-        // TODO:
-        //void set_color_image(image img) // depth, ir
-        //{
-        //	k4a_capture_set_color_image(m_handle, img.handle());
-        //}
+        void set_color_image(const image& color_image)
+        {
+        	k4a_capture_set_color_image(m_handle, color_image.handle());
+        }
+
+        void set_depth_image(const image& depth)
+        {
+        	k4a_capture_set_depth_image(m_handle, depth.handle());
+        }
+
+        void set_ir_image(const image& ir)
+        {
+        	k4a_capture_set_ir_image(m_handle, ir.handle());
+        }
 
         float get_temperature_c() const
         {
@@ -369,15 +431,267 @@ namespace k4a
             capture cap;
             k4a_result_t result = create(cap);
 
-            if (result != K4A_RESULT_SUCCEEDED)
-            {
-                // TODO: Throw exception
-            }
+            check_result(result);
             return cap;
         }
 
     private:
         k4a_capture_t m_handle;
+    };
+
+    struct calibration : public k4a_calibration_t
+    {
+        k4a_result_t convert_3d_to_3d(
+            const k4a_float3_t& source_point3d,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera,
+            k4a_float3_t* target_point3d) const
+        {
+            return k4a_calibration_3d_to_3d(this, &source_point3d, source_camera, target_camera, target_point3d);
+        }
+
+        k4a_float3_t convert_3d_to_3d(
+            const k4a_float3_t& source_point3d,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera) const
+        {
+            k4a_float3_t target_point3d;
+            k4a_result_t result = convert_3d_to_3d(source_point3d, source_camera, target_camera, &target_point3d);
+
+            check_result(result);
+            return target_point3d;
+        }
+
+        k4a_result_t convert_2d_to_3d(
+            const k4a_float2_t& source_point2d,
+            float source_depth,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera,
+            k4a_float3_t* target_point3d,
+            bool* valid)
+        {
+            int intValid = 0;
+            k4a_result_t result = k4a_calibration_2d_to_3d(this, &source_point2d, source_depth, source_camera, target_camera, target_point3d, &intValid);
+
+            if (valid != nullptr)
+            {
+                *valid = intValid != 0;
+            }
+            return result;
+        }
+
+        k4a_float3_t convert_2d_to_3d(
+            const k4a_float2_t& source_point2d,
+            float source_depth,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera,
+            bool* valid = nullptr)
+        {
+            k4a_float3_t target_point3d;
+            k4a_result_t result = convert_2d_to_3d(source_point2d, source_depth, source_camera, target_camera, &target_point3d, valid);
+
+            check_result(result);
+            return target_point3d;
+        }
+
+        k4a_result_t convert_3d_to_2d(
+            const k4a_float3_t& source_point3d,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera,
+            k4a_float2_t *target_point2d,
+            bool* valid)
+        {
+            int intValid = 0;
+            k4a_result_t result = k4a_calibration_3d_to_2d(this, &source_point3d, source_camera, target_camera, target_point2d, &intValid);
+
+            if (valid != nullptr)
+            {
+                *valid = intValid != 0;
+            }
+            return result;
+        }
+
+        k4a_float2_t convert_3d_to_2d(
+            const k4a_float3_t& source_point3d,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera,
+            bool* valid = nullptr)
+        {
+            k4a_float2_t target_point2d;
+            k4a_result_t result = convert_3d_to_2d(source_point3d, source_camera, target_camera, &target_point2d, valid);
+
+            check_result(result);
+            return target_point2d;
+        }
+
+        k4a_result_t convert_2d_to_2d(
+            const k4a_float2_t& source_point2d,
+            float source_depth,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera,
+            k4a_float2_t* target_point2d,
+            bool* valid)
+        {
+            int intValid = 0;
+            k4a_result_t result = k4a_calibration_2d_to_2d(this, &source_point2d, source_depth, source_camera, target_camera, target_point2d, &intValid);
+
+            if (valid != nullptr)
+            {
+                *valid = intValid != 0;
+            }
+            return result;
+        }
+
+        k4a_float2_t convert_2d_to_2d(
+            const k4a_float2_t& source_point2d,
+            float source_depth,
+            k4a_calibration_type_t source_camera,
+            k4a_calibration_type_t target_camera,
+            bool* valid = nullptr)
+        {
+            k4a_float2_t target_point2d;
+            k4a_result_t result = convert_2d_to_2d(source_point2d, source_depth, source_camera, target_camera, &target_point2d, valid);
+
+            check_result(result);
+            return target_point2d;
+        }
+
+        static k4a_result_t get_from_raw(
+            const char* raw_calibration,
+            size_t raw_calibration_size,
+            k4a_depth_mode_t depth_mode,
+            k4a_color_resolution_t color_resolution,
+            calibration& calib)
+        {
+            return k4a_calibration_get_from_raw(const_cast<char*>(raw_calibration), raw_calibration_size, depth_mode, color_resolution, &calib);
+        }
+
+        static k4a_result_t get_from_raw(
+            const uint8_t* raw_calibration,
+            size_t raw_calibration_size,
+            k4a_depth_mode_t depth_mode,
+            k4a_color_resolution_t color_resolution,
+            calibration& calib)
+        {
+            return get_from_raw(reinterpret_cast<const char*>(raw_calibration), raw_calibration_size, depth_mode, color_resolution, calib);
+        }
+
+        static k4a_result_t get_from_raw(
+            const std::vector<uint8_t>& raw_calibration,
+            k4a_depth_mode_t depth_mode,
+            k4a_color_resolution_t color_resolution,
+            calibration& calib)
+        {
+            return get_from_raw(reinterpret_cast<const char*>(raw_calibration.data()), raw_calibration.size(), depth_mode, color_resolution, calib);
+        }
+
+        static calibration get_from_raw(
+            const char* raw_calibration,
+            size_t raw_calibration_size,
+            k4a_depth_mode_t depth_mode,
+            k4a_color_resolution_t color_resolution)
+        {
+            calibration calib;
+            k4a_result_t result = get_from_raw(raw_calibration, raw_calibration_size, depth_mode, color_resolution, calib);
+
+            check_result(result);
+            return calib;
+        }
+
+        static calibration get_from_raw(
+            const uint8_t* raw_calibration,
+            size_t raw_calibration_size,
+            k4a_depth_mode_t depth_mode,
+            k4a_color_resolution_t color_resolution)
+        {
+            calibration calib;
+            k4a_result_t result = get_from_raw(raw_calibration, raw_calibration_size, depth_mode, color_resolution, calib);
+
+            check_result(result);
+            return calib;
+        }
+
+        static calibration get_from_raw(
+            const std::vector<uint8_t>& raw_calibration,
+            k4a_depth_mode_t depth_mode,
+            k4a_color_resolution_t color_resolution)
+        {
+            calibration calib;
+            k4a_result_t result = get_from_raw(raw_calibration, depth_mode, color_resolution, calib);
+
+            check_result(result);
+            return calib;
+        }
+    };
+
+    class transformation
+    {
+    public:
+        transformation(const k4a_calibration_t& calibration)
+            : m_handle(k4a_transformation_create(&calibration))
+        {
+        }
+
+        transformation(k4a_transformation_t handle = nullptr)
+            : m_handle(handle)
+        {
+        }
+
+        transformation(transformation&& other)
+            : m_handle(other.m_handle)
+        {
+            other.m_handle = nullptr;
+        }
+
+        transformation(const transformation&) = delete;
+
+        ~transformation()
+        {
+            destroy();
+        }
+
+        transformation& operator=(transformation&& other)
+        {
+            destroy();
+            m_handle = other.m_handle;
+            other.m_handle = nullptr;
+            return *this;
+        }
+
+        transformation& operator=(std::nullptr_t)
+        {
+            destroy();
+            return *this;
+        }
+
+        transformation& operator=(const transformation&) = delete;
+
+        void destroy()
+        {
+            if (m_handle != nullptr)
+            {
+                k4a_transformation_destroy(m_handle);
+                m_handle = nullptr;
+            }
+        }
+
+        k4a_result_t depth_image_to_color_camera(const image& depth_image, image& transformed_depth_image) const
+        {
+            return k4a_transformation_depth_image_to_color_camera(m_handle, depth_image.handle(), transformed_depth_image.handle());
+        }
+
+        k4a_result_t color_image_to_depth_camera(const image& depth_image, const image& color_image, image& transformed_color_image) const
+        {
+            return k4a_transformation_color_image_to_depth_camera(m_handle, depth_image.handle(), color_image.handle(), transformed_color_image.handle());
+        }
+
+        k4a_result_t depth_image_to_point_cloud(const image& depth_image, k4a_calibration_type_t camera, image& xyz_image) const
+        {
+            return k4a_transformation_depth_image_to_point_cloud(m_handle, depth_image.handle(), camera, xyz_image.handle());
+        }
+
+    private:
+        k4a_transformation_t m_handle;
     };
 
     class device
@@ -500,17 +814,15 @@ namespace k4a
 
             // On some prototype devices, we get back an empty serial number (just the '\0').
             // If that happens, we want to report 'unknown device' rather than not printing an identifier.
-            //
             if (result == K4A_BUFFER_RESULT_TOO_SMALL && buffer > 1)
             {
                 serialnum.resize(buffer);
                 result = get_serialnum(&serialnum[0], &buffer);
-                if (result == K4A_BUFFER_RESULT_SUCCEEDED)
+                if (result == K4A_BUFFER_RESULT_SUCCEEDED && serialnum[buffer - 1] == 0)
                 {
                     // std::string expects there to not be as null terminator at the end of its data but
                     // k4a_device_get_serialnum adds a null terminator, so we drop the last character of the string after we get
                     // the result back.
-                    //
                     serialnum.resize(buffer - 1);
                 }
             }
@@ -532,9 +844,64 @@ namespace k4a
             return k4a_device_set_color_control(m_handle, command, mode, value);
         }
 
+        k4a_buffer_result_t get_raw_calibration(uint8_t *data, size_t *data_size) const
+        {
+            return k4a_device_get_raw_calibration(m_handle, data, data_size);
+        }
+
+        std::vector<uint8_t> get_raw_calibration() const
+        {
+            std::vector<uint8_t> calibration;
+            size_t buffer = 0;
+            k4a_buffer_result_t result = get_raw_calibration(&calibration[0], &buffer);
+
+            if (result == K4A_BUFFER_RESULT_TOO_SMALL && buffer > 1)
+            {
+                calibration.resize(buffer);
+                result = get_raw_calibration(&calibration[0], &buffer);
+                if (result != K4A_BUFFER_RESULT_SUCCEEDED)
+                {
+                    calibration.clear();
+                }
+            }
+            return calibration;
+        }
+
+        k4a_result_t get_calibration(k4a_depth_mode_t mode, k4a_color_resolution_t resolution, calibration* calib) const
+        {
+            return k4a_device_get_calibration(m_handle, mode, resolution, calib);
+        }
+
+        calibration get_calibration(k4a_depth_mode_t mode, k4a_color_resolution_t resolution) const
+        {
+            calibration calib;
+            k4a_result_t result = get_calibration(mode, resolution, &calib);
+
+            check_result(result);
+            return calib;
+        }
+
         k4a_result_t get_sync_jack(bool *sync_in_jack_connected, bool *sync_out_jack_connected) const
         {
             return k4a_device_get_sync_jack(m_handle, sync_in_jack_connected, sync_out_jack_connected);
+        }
+
+        bool is_sync_in_jack_connected() const
+        {
+            bool sync_in_jack_connected, sync_out_jack_connected;
+            k4a_result_t result = get_sync_jack(&sync_in_jack_connected, &sync_out_jack_connected);
+
+            check_result(result);
+            return sync_in_jack_connected;
+        }
+
+        bool is_sync_out_jack_connected() const
+        {
+            bool sync_in_jack_connected, sync_out_jack_connected;
+            k4a_result_t result = get_sync_jack(&sync_in_jack_connected, &sync_out_jack_connected);
+
+            check_result(result);
+            return sync_out_jack_connected;
         }
 
         k4a_result_t get_version(k4a_hardware_version_t *version) const
