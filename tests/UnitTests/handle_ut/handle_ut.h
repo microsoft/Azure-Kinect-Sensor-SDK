1c720deeddd30f816ccee44a3b7686302c7d20fa
wes-b <wesbarc@microsoft.com>
2019-03-20 10:14:58 -0700

    Add CPP support to K4A_DECLARE_CONTEXT (#149)
    * When compiled into a CPP file new/delete are used for the context.
    When called from a C file malloc/free are used. UT's have been
    added to confirm this.
    
    * addressing comments
    
    * addressing build error, nothrow appears to be a CPP17 feature
    
    * adding nothrow back
    
    * reverting nothrow from record tests, this was unintentional
    
    * Single allocation

--- tests/UnitTests/handle_ut/handle_ut.h
+++ tests/UnitTests/handle_ut/handle_ut.h
@@ -1,10 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-#include <utcommon.h>
+#ifdef __cplusplus
+#include <cinttypes>
+#else
+#include <inttypes.h>
+#endif
 
 // Module being tested
 #include <k4ainternal/handle.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 K4A_DECLARE_HANDLE(dual_defined_t);
 
@@ -19,3 +27,8 @@ K4A_DECLARE_CONTEXT(dual_defined_t, dual_defined_context_t);
 
 // Shared function prototype
 int is_handle_in_2nd_file_valid(dual_defined_t handle);
+int is_handle_in_c_file_valid(dual_defined_t handle);
+
+#ifdef __cplusplus
+}
+#endif
