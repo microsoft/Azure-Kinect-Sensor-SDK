5634cfcfe053e56740a0f7f5e2b302ded7f8fb5f
Andrew Duan <axduan@microsoft.com>
2019-03-22 16:17:06 -0700

    Small fix

--- src/dewrapper/dewrapper.c
+++ src/dewrapper/dewrapper.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// This library Depth Engine Wrapper
+// This library
 #include <k4ainternal/dewrapper.h>
 
 // Dependent libraries
