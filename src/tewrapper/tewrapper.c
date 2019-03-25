66afa23d9cdd013c61c681184ed98b2e50cb1c74
Andrew Duan <axduan@microsoft.com>
2019-03-25 09:53:57 -0700

    Being back two comments

--- src/tewrapper/tewrapper.c
+++ src/tewrapper/tewrapper.c
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-// This library
+// This library is Transform Engine Wrapper
 #include <k4ainternal/tewrapper.h>
 
 // Dependent libraries
