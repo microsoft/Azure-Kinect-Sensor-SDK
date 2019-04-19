d3e2d49f0ad889028f55a32ce4be7fdd095c5559
Derek Mantey <derekma@microsoft.com>
2019-04-19 15:20:04 -0700

    Removing the JSON implementation of the environment info.

--- src/csharp/Microsoft.AzureKinect.Test.StubGenerator/EnvironmentInfo.cs
+++ src/csharp/Microsoft.AzureKinect.Test.StubGenerator/EnvironmentInfo.cs
@@ -15,43 +15,24 @@ namespace Microsoft.AzureKinect.Test.StubGenerator
                 return;
             }
 
-            try
+            lock (EnvironmentInfo.SyncRoot)
             {
-                lock (EnvironmentInfo.SyncRoot)
+                if (EnvironmentInfo.IsInitialized)
                 {
-                    if (EnvironmentInfo.IsInitialized)
-                    {
-                        return;
-                    }
-
-                    Console.WriteLine("LoadEnvironment...");
-
-                    //string appSettingsJson = File.ReadAllText(@"appsettings.json");
-                    //JObject appSettings = JObject.Parse(appSettingsJson);
-
-                    //Environment.SetEnvironmentVariable("K4A_BINARY_DIR", appSettings["Paths"]["K4A_BINARY_DIR"].Value<string>());
-                    //Environment.SetEnvironmentVariable("K4A_SOURCE_DIR", appSettings["Paths"]["K4A_SOURCE_DIR"].Value<string>());
-                    //Environment.SetEnvironmentVariable("CMAKE_CXX_COMPILER", appSettings["Paths"]["CMAKE_CXX_COMPILER"].Value<string>());
-                    //Environment.SetEnvironmentVariable("CMAKE_LINKER", appSettings["Paths"]["CMAKE_LINKER"].Value<string>());
-                    //Environment.SetEnvironmentVariable("INCLUDE", appSettings["Paths"]["INCLUDE"].Value<string>());
-                    //Environment.SetEnvironmentVariable("LIB", appSettings["Paths"]["LIB"].Value<string>());
-
-                    XmlDocument appSettings = new XmlDocument();
-                    appSettings.Load("appsettings.xml");
-
-                    Environment.SetEnvironmentVariable("K4A_BINARY_DIR", appSettings.DocumentElement["K4A_BINARY_DIR"].InnerXml);
-                    Environment.SetEnvironmentVariable("K4A_SOURCE_DIR", appSettings.DocumentElement["K4A_SOURCE_DIR"].InnerXml);
-                    Environment.SetEnvironmentVariable("CMAKE_CXX_COMPILER", appSettings.DocumentElement["CMAKE_CXX_COMPILER"].InnerXml);
-                    Environment.SetEnvironmentVariable("CMAKE_LINKER", appSettings.DocumentElement["CMAKE_LINKER"].InnerXml);
-                    Environment.SetEnvironmentVariable("INCLUDE", appSettings.DocumentElement["INCLUDE"].InnerXml);
-                    Environment.SetEnvironmentVariable("LIB", appSettings.DocumentElement["LIB"].InnerXml);
-
-                    EnvironmentInfo.IsInitialized = true;
+                    return;
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Failed to load settings: {0}", ex);
+
+                XmlDocument appSettings = new XmlDocument();
+                appSettings.Load("appsettings.xml");
+
+                Environment.SetEnvironmentVariable("K4A_BINARY_DIR", appSettings.DocumentElement["K4A_BINARY_DIR"].InnerXml);
+                Environment.SetEnvironmentVariable("K4A_SOURCE_DIR", appSettings.DocumentElement["K4A_SOURCE_DIR"].InnerXml);
+                Environment.SetEnvironmentVariable("CMAKE_CXX_COMPILER", appSettings.DocumentElement["CMAKE_CXX_COMPILER"].InnerXml);
+                Environment.SetEnvironmentVariable("CMAKE_LINKER", appSettings.DocumentElement["CMAKE_LINKER"].InnerXml);
+                Environment.SetEnvironmentVariable("INCLUDE", appSettings.DocumentElement["INCLUDE"].InnerXml);
+                Environment.SetEnvironmentVariable("LIB", appSettings.DocumentElement["LIB"].InnerXml);
+
+                EnvironmentInfo.IsInitialized = true;
             }
         }
     }
