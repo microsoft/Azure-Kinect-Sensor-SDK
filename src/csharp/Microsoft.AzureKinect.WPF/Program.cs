5996d8cc6ff9eed9c934c5588f594cc387c84ca6
Derek M <D1973@rocketmail.com>
2019-05-01 16:08:57 -0700

    Create Program.cs
    This program.cs file should be large enough to trigger CLA.

---
+++ src/csharp/Microsoft.AzureKinect.WPF/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using Microsoft.AzureKinect.Test.StubGenerator;
+
+namespace StubValidator
+{
+    /// <summary>
+    /// This is the main program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// This is the main entry point into the main program.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am not sure why this change would make it in.");
+        }
+    }
+}
