7e3ca89bd102c503f3f1c7ac618446e9b133c15f
Derek Mantey <derekma@microsoft.com>
2019-06-27 16:14:14 -0700

    Move the intellisense file to the output directory. Fixing spelling.

--- src/csharp/Microsoft.Azure.Kinect.Sensor/NativeMethods.cs
+++ src/csharp/Microsoft.Azure.Kinect.Sensor/NativeMethods.cs
@@ -12,9 +12,8 @@ namespace Microsoft.Azure.Kinect.Sensor
 
     internal class NativeMethods
     {
-        // These types are used internally by the interop dll for marshalling purposes and are not exposed
+        // These types are used internally by the interop dll for marshaling purposes and are not exposed
         // over the public surface of the managed dll. 
-
 
         #region Handle Types
 
