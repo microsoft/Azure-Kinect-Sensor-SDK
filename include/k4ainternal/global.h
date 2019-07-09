0caee4bc3490f7cefa99f0c1ca4ee331d3d1b7bc
Brent Allen <brenta@microsoft.com>
2019-07-09 16:40:47 -0700

    Added one time init and converted logger to use it

---
+++ include/k4ainternal/global.h
@@ -0,0 +1,50 @@
+/** \file global.h
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ * Kinect For Azure SDK.
+ */
+
+#ifndef GLOBAL_H 
+#define GLOBAL_H
+
+#include <k4a/k4atypes.h>
+#include <k4ainternal/rwlock.h>
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef _WIN32
+typedef INIT_ONCE k4a_init_once_t;
+#define K4A_INIT_ONCE INIT_ONCE_STATIC_INIT
+#else
+typedef pthread_once_t k4a_init_once_t;
+#define K4A_INIT_ONCE PTHREAD_ONCE_INIT
+#endif
+
+typedef int (k4a_init_once_function_t)();
+
+void global_init_once(k4a_init_once_t* init_once, k4a_init_once_function_t* init_function);
+
+#define K4A_DECLARE_GLOBAL(_global_type_, _init_function_) \
+    static k4a_init_once_t g_ ## _global_type_ ## _init_once = K4A_INIT_ONCE; \
+    static _global_type_ _ ## _global_type_ ## _private; \
+    static int fn_ ## _global_type_ ## _init_function() \
+    { \
+        memset(&_ ## _global_type_ ## _private, 0, sizeof(_ ## _global_type_ ## _private)); \
+        _init_function_(&_ ## _global_type_ ## _private); \
+        return 0; \
+    } \
+    static _global_type_* _global_type_ ## _get() \
+    { \
+        global_init_once(&g_ ## _global_type_ ## _init_once, fn_ ## _global_type_ ## _init_function); \
+        return &_ ## _global_type_ ## _private; \
+    }
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GLOBAL_H */
---
+++ src/global/global.c
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// This library
+#include <k4ainternal/global.h>
+
+// Dependent libraries
+#include <k4ainternal/capture.h>
+#include <azure_c_shared_utility/lock.h>
+#include <azure_c_shared_utility/refcount.h>
+#include <azure_c_shared_utility/threadapi.h>
+
+// System dependencies
+#include <stdlib.h>
+#include <assert.h>
+
+
+#ifdef _WIN32
+static INIT_ONCE g_InitOnce = INIT_ONCE_STATIC_INIT;
+
+static BOOL CALLBACK InitGlobalFunction(
+    PINIT_ONCE InitOnce,
+    PVOID Parameter,
+    PVOID *lpContext)
+{
+    UNREFERENCED_PARAMETER(InitOnce);
+    UNREFERENCED_PARAMETER(lpContext);
+
+    k4a_init_once_function_t* callback = (k4a_init_once_function_t*)Parameter;
+
+    callback();
+
+    return TRUE;
+}
+#endif
+
+void global_init_once(k4a_init_once_t* init_once, k4a_init_once_function_t* init_function)
+{
+
+#ifdef _WIN32
+    if (InitOnceExecuteOnce(init_once,
+        InitGlobalFunction,
+        (void*)init_function,
+        NULL))
+    {
+        return;
+    }
+#else
+    if (0 == pthread_once(init_once,
+        init_function))
+    {
+        return;
+    }
+#endif
+
+    assert(false);
+    return;
+}
+
+
