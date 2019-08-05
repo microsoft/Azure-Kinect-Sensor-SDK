2aece9abcc8083d69c35ab3c0f92981f55000b2f
Brent Allen <brenta@microsoft.com>
2019-08-05 15:23:09 -0700

    Renamed calibration types

---
+++ src/csharp/SDK/CalibrationDeviceType.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Azure.Kinect.Sensor
+{
+    [Native.NativeReference("k4a_calibration_type_t")]
+    public enum CalibrationDeviceType
+    {
+        [Native.NativeReference("K4A_CALIBRATION_TYPE_UNKNOWN")]
+        Unknown = -1,
+        [Native.NativeReference("K4A_CALIBRATION_TYPE_DEPTH")]
+        Depth,
+        [Native.NativeReference("K4A_CALIBRATION_TYPE_COLOR")]
+        Color,
+        [Native.NativeReference("K4A_CALIBRATION_TYPE_GYRO")]
+        Gyro,
+        [Native.NativeReference("K4A_CALIBRATION_TYPE_ACCEL")]
+        Accel,
+        [Native.NativeReference("K4A_CALIBRATION_TYPE_NUM")]
+        Num
+    }
+}
---
+++ src/csharp/SDK/CameraCalibration.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Azure.Kinect.Sensor
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct CameraCalibration
+    {
+        [MarshalAs(UnmanagedType.Struct)]
+        public Extrinsics extrinsics;
+
+        [MarshalAs(UnmanagedType.Struct)]
+        public Intrinsics intrinsics;
+
+        public int resolution_width;
+        public int resolution_height;
+        public float metric_radius;
+    }
+}
