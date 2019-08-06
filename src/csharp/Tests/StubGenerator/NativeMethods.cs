3da323691af1d6212f893b3a113b6fcf7346f0c2
Brent Allen <brenta@microsoft.com>
2019-08-06 13:26:48 -0700

    Fixed x86 calling conventions in C# (#578)
    * Fixed calling convention for x86
    
    * Added GC to main loop for WPF on x86

--- src/csharp/Tests/StubGenerator/NativeMethods.cs
+++ src/csharp/Tests/StubGenerator/NativeMethods.cs
@@ -7,10 +7,12 @@ namespace Microsoft.Azure.Kinect.Sensor.Test.StubGenerator
 {
     static class NativeMethods
     {
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        private const CallingConvention stubCallingConvention = CallingConvention.Cdecl;
+
+        [UnmanagedFunctionPointer(stubCallingConvention, CharSet = CharSet.Ansi)]
         public delegate void RaiseError([MarshalAs(UnmanagedType.LPStr)]string szFile, int line, [MarshalAs(UnmanagedType.LPStr)]string expression);
 
-        [UnmanagedFunctionPointer(CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+        [UnmanagedFunctionPointer(stubCallingConvention, CharSet = CharSet.Ansi)]
         public delegate void Stub_SetErrorFunction(RaiseError errorFunction);
 
         [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
@@ -22,10 +24,10 @@ namespace Microsoft.Azure.Kinect.Sensor.Test.StubGenerator
         [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
         public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
 
-        [DllImport("k4a", CharSet = CharSet.Ansi)]
+        [DllImport("k4a", CharSet = CharSet.Ansi, CallingConvention=stubCallingConvention)]
         public static extern int Stub_RegisterRedirect(string functionName, IntPtr implementation);
 
-        [DllImport("k4a", CharSet = CharSet.Ansi)]
+        [DllImport("k4a", CharSet = CharSet.Ansi, CallingConvention=stubCallingConvention)]
         public static extern int Stub_GetCallCount(string functionName);
 
         
