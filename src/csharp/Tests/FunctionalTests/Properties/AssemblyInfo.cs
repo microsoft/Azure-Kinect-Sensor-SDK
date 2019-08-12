f64b89b5e018efd61d807e3a543d28894d4cd811
Derek M <MsDerekMa@users.noreply.github.com>
2019-08-12 16:35:30 -0700

    Csharp version (#635)
    * Switch .NET Standard and .NET Core projects to using AssemblyInfo and VersionInfo.
    * Generate the VersionInfo file curing the CMake configure step.

--- src/csharp/Tests/FunctionalTests/Properties/AssemblyInfo.cs
+++ src/csharp/Tests/FunctionalTests/Properties/AssemblyInfo.cs
@@ -4,15 +4,15 @@
 // Licensed under the MIT License.
 // </copyright>
 //------------------------------------------------------------------------------
-using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.Kinect.Sensor.WinForms")]
+[assembly: AssemblyTitle("Microsoft.Azure.Kinect.Sensor.FunctionalTests")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("618e28c5-0624-463f-9adb-040ba5daed68")]
+[assembly: Guid("ebf80b3b-83a1-4369-954b-922d8ee5e9cc")]
--- src/csharp/Tests/UnitTests/Properties/AssemblyInfo.cs
+++ src/csharp/Tests/UnitTests/Properties/AssemblyInfo.cs
@@ -4,15 +4,15 @@
 // Licensed under the MIT License.
 // </copyright>
 //------------------------------------------------------------------------------
-using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.Kinect.Sensor.WinForms")]
+[assembly: AssemblyTitle("Microsoft.Azure.Kinect.Sensor.UnitTests")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("618e28c5-0624-463f-9adb-040ba5daed68")]
+[assembly: Guid("9a8ec674-aad4-486b-b747-d53989bc8f89")]
