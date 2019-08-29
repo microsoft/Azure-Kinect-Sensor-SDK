c5046497805bd3d2778e04633078b8beb62bd300
Andrew Duan <axduan@microsoft.com>
2019-08-29 09:22:15 -0700

    Fix clang build

--- src/transformation/transformation.c
+++ src/transformation/transformation.c
@@ -368,7 +368,7 @@ k4a_result_t transformation_color_2d_to_depth_2d(const k4a_calibration_t *calibr
     // distance from the input color pixel coordinates
     int depth_image_width_pixels = image_get_width_pixels(depth_image);
     int depth_image_height_pixels = image_get_height_pixels(depth_image);
-    uint16_t *depth_image_data = (uint16_t *)(image_get_buffer(depth_image));
+    const uint16_t *depth_image_data = (const uint16_t *)(const void *)(image_get_buffer(depth_image));
     float best_error = FLT_MAX;
     float p[2];
     p[0] = start_point2d[0];
