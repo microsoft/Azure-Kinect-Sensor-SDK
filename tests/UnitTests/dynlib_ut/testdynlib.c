74c0c1c61603223d66fe01db35a863012d805dfa
Matt Schulte <maschu@microsoft.com>
2019-09-13 14:41:05 -0700

    Properly Sets default visibility of functions (#763)
    * Properly Sets default visibility of functions
    
    * Export functions in dynlib unit tests
    
    * Removed old macros
    
    * Add documentation to the policy
    
    * Fixed typo

--- tests/UnitTests/dynlib_ut/testdynlib.c
+++ tests/UnitTests/dynlib_ut/testdynlib.c
@@ -1,16 +1,13 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+#include "test_dynlib_export.h"
+
 #include <stdio.h>
 
-#ifndef _WIN32
-#define __declspec(arg)
-#define __cdecl
-#endif
+TEST_DYNLIB_EXPORT void say_hello(void);
 
-__declspec(dllexport) void __cdecl say_hello(void);
-
-__declspec(dllexport) void __cdecl say_hello(void)
+TEST_DYNLIB_EXPORT void say_hello(void)
 {
     printf("Hello!\n");
 }
