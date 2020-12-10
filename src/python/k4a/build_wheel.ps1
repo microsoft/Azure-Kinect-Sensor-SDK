ad64501a7e7a5d7b2580341107c9f0af0183c465
Jonathan Santos <jonsanto@microsoft.com>
2020-12-10 10:44:06 -0800

    Python k4a api: Adding a check that the required dlls are in the _libs folder before making the wheel.

--- src/python/k4a/build_wheel.ps1
+++ src/python/k4a/build_wheel.ps1
@@ -1,4 +1,27 @@
+# Script to automate creation of the Python wheel for the k4a library.
+
+# Check that the k4a.dll and depth_engine_2_0.dll have been copied into the _libs/ folder.
+# Because these dlls may not be located in a standard place, leave it to the developer
+# to manually copy them into the _libs/ folder.
+If (-not (Test-Path -Path "$PSScriptRoot\src\k4a\_libs\k4a.dll")) {
+    Write-Host "File not found: $PSScriptRoot\src\k4a\_libs\k4a.dll"
+    Write-Host "Please manually copy the dll into that folder."
+    exit 1
+}
+
+If (-not(Test-Path -Path "$PSScriptRoot\src\k4a\_libs\depthengine*.dll")) {
+    Write-Host "File not found: $PSScriptRoot\src\k4a\_libs\depthengine*.dll"
+    Write-Host "Please manually copy the dll into that folder."
+    exit 1
+}
+
 # Create a virtual environment and activate it.
+Write-Host "Creating a Python virtual environment."
+
+If (Test-Path -Path "temp_build_venv") {
+    Remove-Item -LiteralPath "temp_build_venv" -Force -Recurse
+}
+
 python -m venv temp_build_venv
 ./temp_build_venv/Scripts/activate
 
