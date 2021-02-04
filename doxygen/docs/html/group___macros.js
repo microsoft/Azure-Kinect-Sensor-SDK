60e04758f0a3fea79c3348e2e77c78723fcea8fd
Jonathan Santos <jonsanto@microsoft.com>
2021-02-03 23:43:34 -0800

    SDK C API: Removing doxygen output files. These files should not be checked in since they can be easily regenerated from source.

--- doxygen/docs/html/group___macros.js
+++
@@ -1,6 +0,0 @@
-var group___macros =
-[
-    [ "K4A_FAILED", "group___macros_ga7c2e32349135d008b6f836c571d434b4.html#ga7c2e32349135d008b6f836c571d434b4", null ],
-    [ "K4A_INIT_STRUCT", "group___macros_gaeab021a840259ca3ad882bd080260224.html#gaeab021a840259ca3ad882bd080260224", null ],
-    [ "K4A_SUCCEEDED", "group___macros_ga8e5b48150bc243c6052793bd830c2fcd.html#ga8e5b48150bc243c6052793bd830c2fcd", null ]
-];
--- doxygen/docs/html/structk4a__version__t.js
+++
@@ -1,6 +0,0 @@
-var structk4a__version__t =
-[
-    [ "iteration", "structk4a__version__t.html#a9f3ec6a118f81533c7748b9080f68acf", null ],
-    [ "major", "structk4a__version__t.html#aba947b238819496adc41888de36df6e1", null ],
-    [ "minor", "structk4a__version__t.html#a52626a80e919cac31d837efa3d01abfb", null ]
-];
