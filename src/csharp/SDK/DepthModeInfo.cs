a284750284915ba8dd227db30b0cc75d95f1e93b
AntonClaytonBursch <v-abursch@microsoft.com>
2021-02-05 08:39:35 -0800

    new C# classes and native methods are commented properly

--- src/csharp/SDK/DepthModeInfo.cs
+++ src/csharp/SDK/DepthModeInfo.cs
@@ -10,34 +10,86 @@ using static Microsoft.Azure.Kinect.Sensor.NativeMethods;
 
 namespace Microsoft.Azure.Kinect.Sensor
 {
+    /// <summary>
+    /// Represents the configuration to run a depth device in.
+    /// </summary>
+    /// <remarks>
+    /// Default initialization is the same as K4A_DEVICE_CONFIG_INIT_DISABLE_ALL.
+    /// </remarks>
     public class DepthModeInfo
     {
+        /// <summary>
+        /// Gets or sets the Struct Size.
+        /// </summary>
         public int StructSize { get; set; } = 52;
 
+        /// <summary>
+        /// Gets or sets the Struct Version.
+        /// </summary>
         public int StructVersion { get; set; } = 1;
 
+        /// <summary>
+        /// Gets or sets the ModeId of the depth camera.
+        /// </summary>
         public int ModeId { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to use Passive IR Only.
+        /// </summary>
+        /// <remarks>
+        /// If this is false, the Passive IR images are dropped.
+        /// </remarks>
         public bool PassiveIROnly { get; set; } = false;
 
+        /// <summary>
+        /// Gets or sets the image width.
+        /// </summary>
         public int Width { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets the image height.
+        /// </summary>
         public int Height { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets the image format. Depth image type DEPTH16. Each pixel of DEPTH16 data is two bytes of little-endian unsigned depth data.
+        /// </summary>
         public ImageFormat NativeFormat { get; set; } = ImageFormat.Depth16;
 
+        /// <summary>
+        /// Gets or sets the Horizontal FOV.
+        /// </summary>
         public float HorizontalFOV { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets the Vertical FOV.
+        /// </summary>
         public float VerticalFOV { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets the Min FPS.
+        /// </summary>
         public int MinFPS { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets the Max FPS.
+        /// </summary>
         public int MaxFPS { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets the Min Range.
+        /// </summary>
         public int MinRange { get; set; } = 0;
 
+        /// <summary>
+        /// Gets or sets the Max Range.
+        /// </summary>
         public int MaxRange { get; set; } = 0;
 
+        /// <summary>
+        /// Gets equivalent native configuration struct.
+        /// </summary>
+        /// <returns>k4a_depth_mode_info_t.</returns>
         internal k4a_depth_mode_info_t GetNativeConfiguration()
         {
             return new k4a_depth_mode_info_t
