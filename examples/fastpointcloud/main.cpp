611f9b1df32b30c9fac41c2ea0332f199df524c7
AntonClaytonBursch <v-abursch@microsoft.com>
2021-02-11 00:31:38 -0800

    cleaning up the examples

--- examples/fastpointcloud/main.cpp
+++ examples/fastpointcloud/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+using namespace std;
 
 static void create_xy_table(const k4a_calibration_t *calibration, k4a_image_t xy_table)
 {
@@ -123,6 +124,10 @@ int main(int argc, char **argv)
     k4a_image_t point_cloud = NULL;
     int point_count = 0;
 
+    // 1. declare depth and color modes
+    k4a_depth_mode_info_t depth_mode_info = { sizeof(k4a_depth_mode_info_t), K4A_ABI_VERSION, 0 };
+    k4a_fps_mode_info_t fps_mode_info = { sizeof(k4a_fps_mode_info_t), K4A_ABI_VERSION, 0 };
+
     if (argc != 2)
     {
         printf("fastpointcloud.exe <output file>\n");
@@ -146,8 +151,69 @@ int main(int argc, char **argv)
         goto Exit;
     }
 
-    config.depth_mode_id = 3; // K4A_DEPTH_MODE_WFOV_2X2BINNED
-    config.fps_mode_id = 30;  // K4A_FRAMES_PER_SECOND_30
+
+    // 2. initialize default mode ids
+    uint32_t color_mode_id = 0;
+    uint32_t depth_mode_id = 0;
+    uint32_t fps_mode_id = 0;
+
+    // 3. get the count of modes
+    uint32_t depth_mode_count = 0;
+    uint32_t fps_mode_count = 0;
+
+    // 4. find the mode ids you want
+    if (depth_mode_count > 0)
+    {
+        for (uint32_t d = 1; d < depth_mode_count; d++)
+        {
+            k4a_depth_mode_info_t depth_mode = { sizeof(k4a_depth_mode_info_t), K4A_ABI_VERSION, 0 };
+            if (k4a_device_get_depth_mode(device, d, &depth_mode) == K4A_RESULT_SUCCEEDED)
+            {
+                if (depth_mode.height >= 512 && depth_mode.vertical_fov >= 120)
+                {
+                    depth_mode_id = d;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (fps_mode_count > 0)
+    {
+        uint32_t max_fps = 0;
+        for (uint32_t f = 1; f < fps_mode_count; f++)
+        {
+            k4a_fps_mode_info_t fps_mode = { sizeof(k4a_fps_mode_info_t), K4A_ABI_VERSION, 0 };
+            if (k4a_device_get_fps_mode(device, f, &fps_mode) == K4A_RESULT_SUCCEEDED)
+            {
+                if (fps_mode.fps >= (int)max_fps)
+                {
+                    max_fps = (uint32_t)fps_mode.fps;
+                    fps_mode_id = f;
+                }
+            }
+        }
+    }
+
+    // 5. fps mode id must not be set to 0, which is Off, and either color mode id or depth mode id must not be set to 0
+    if (fps_mode_id == 0)
+    {
+        cout << "Fps mode id must not be set to 0 (Off)" << endl;
+        exit(-1);
+    }
+
+    if (color_mode_id == 0 && depth_mode_id == 0)
+    {
+        cout << "Either color mode id or depth mode id must not be set to 0 (Off)" << endl;
+        exit(-1);
+    }
+
+    // 6. use the mode ids to get the modes
+    k4a_device_get_depth_mode(device, depth_mode_id, &depth_mode_info);
+    k4a_device_get_fps_mode(device, fps_mode_id, &fps_mode_info);
+
+    config.depth_mode_id = depth_mode_info.mode_id;
+    config.fps_mode_id = fps_mode_info.mode_id;
 
     k4a_calibration_t calibration;
     if (K4A_RESULT_SUCCEEDED !=
