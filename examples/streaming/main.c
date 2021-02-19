0250986ebb7597bdd8b3d2753259adb328cb3325
AntonClaytonBursch <v-abursch@microsoft.com>
2021-02-19 11:16:55 -0800

    get mode id from mode info structs in get mode ids functions in examples

--- examples/streaming/main.c
+++ examples/streaming/main.c
@@ -60,7 +60,7 @@ get_device_mode_ids(k4a_device_t device, uint32_t *color_mode_id, uint32_t *dept
             {
                 if (color_mode_info.height >= 2160)
                 {
-                    *color_mode_id = c;
+                    *color_mode_id = color_mode_info.mode_id;
                     break;
                 }
             }
@@ -75,7 +75,7 @@ get_device_mode_ids(k4a_device_t device, uint32_t *color_mode_id, uint32_t *dept
             {
                 if (depth_mode_info.height >= 576 && depth_mode_info.vertical_fov <= 65)
                 {
-                    *depth_mode_id = d;
+                    *depth_mode_id = depth_mode_info.mode_id;
                     break;
                 }
             }
@@ -92,7 +92,7 @@ get_device_mode_ids(k4a_device_t device, uint32_t *color_mode_id, uint32_t *dept
                 if (fps_mode_info.fps >= (int)max_fps)
                 {
                     max_fps = (uint32_t)fps_mode_info.fps;
-                    *fps_mode_id = f;
+                    *fps_mode_id = fps_mode_info.mode_id;
                 }
             }
         }
